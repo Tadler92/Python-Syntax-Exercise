27cdaef3b4871d7f2589a03b52b37e963a2b3d66
Trevor Duhon <taduhon1485@gmail.com>
2024-01-14 00:40:10 -0600

    Changed code so that the function only printed out uppercased words that start with the letter e (whether upper or lowercased)

--- words.py
+++ words.py
@@ -2,4 +2,5 @@ def print_upper_words(words):
     """Prints out an uppercased version of words in a list"""
 
     for word in words:
-        print(word.upper())
+        if word.lower().startswith('e'):
+          print(word.upper())
